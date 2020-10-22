1ae9b7877537225db900514434c7fdccb46516c3
briannguyen4 <brian4nguyen@gmail.com>
2020-10-22 11:21:00 -0400

    seed

--- src/snippets.js
+++ src/snippets.js
@@ -7,147 +7,142 @@ export const snippets = {
     },
 
     2: {
-        text: "Billie Jean is not my lover. She's just a girl who claims that I am the one, but the kid is not my son.",
-        info: "'Billie Jean' by Michael Jackson - 1982"
+        text: "She's just a girl who claims that I am the one, but the kid is not my son.",
+        title: "'Billie Jean' by Michael Jackson - 1982"
     },
 
     3: {
         text: "And you can tell everybody this is your song. It may be quite simple but now that it's done.",
-        info: "'Your Song' by Elton John - 1970"
+        title: "'Your Song' by Elton John - 1970"
     },
 
     4: {
         text: "If there's a bustle in your hedgerow, don't be alarmed now. It's just a spring clean for the May queen.",
-        info: "'Stairway to Heaven' by Led Zeppelin - 1971"
+        title: "'Stairway to Heaven' by Led Zeppelin - 1971"
     },
 
     5: {
-        text: "If I should stay, I would only be in your way. So I'll go, but I know I'll think of you every step of the way.",
-        info: "'I Will Always Love You' by Whitney Houston - 1992"
+        text: "Been working so hard, I'm punching my card. Eight hours, for what?",
+        title: "Footloose"
     },
 
     6: {
         text: "But I'm a creep. I'm a weirdo. What the hell am I doing here? I don't belong here.",
-        info: "'Creep' by Radiohead - 1992"
+        title: "'Creep' by Radiohead - 1992"
     },
 
     7: {
-        text: "Oh, my love, my darling. I've hungered for your touch a long, lonely time.",
-        info: "'Unchained Melody' by The Righteous Brothers - 1965"
+        text: "Oh, my love, my darling I've hungered for your touch a long, lonely time.",
+        title: "'Unchained Melody' by The Righteous Brothers - 1965"
     },
 
     8: {
         text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        title: "'Blackbird' by The Beatles - 1968"
     },
 
     9: {
         text: "I never thought through love we'd be making one as lovely as she. But isn't she lovely made from love?",
-        info: "'Isn\'t She Lovely' by Stevie Wonder - 1976"
+        title: "'Isn\'t She Lovely' by Stevie Wonder - 1976"
     },
 
     10: {
         text: "Life is a mystery. Everyone must stand alone. I hear you call my name and it feels like home.",
-        info: "'Like A Prayer' by Madonna - 1989"
+        title: "'Like A Prayer' by Madonna - 1989"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "A singer in a smoky room, the smell of wine and cheap perfume.",        
+        title: "Don't Stop Believin'"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "Oh! I wanna dance with somebody. I wanna feel the heat with somebody.",
+        title: "I Wanna Dance With Somebody (Who Loves Me)"
+    },
+    8: {
+        text: "Oh momma dear, we're not the fortunate ones. And girls, they wanna have fun.",
+        title: "Girls Just Want to Have Fun"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
-    },
-    8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "Don't you know now is the perfect time. We can make it right, hit the city lights.",
+        title: "P.Y.T. (Pretty Young Thing)"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "Jessie is a friend. Yeah, I know, he's been a good friend of mine.",
+        title: "Jessie's Girl"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "I've been waiting for so long, now I've finally found someone to stand by me.",
+        title: "(I’ve Had) The Time of My Life"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "A scrub is a guy that thinks he's fly. He's also known as a busta.",
+        title: "No Scrubs"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "It's tearin' up my heart when I'm with you. But when we are apart, I feel it too.",
+        title: "Tearin' Up My Heart"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "Load up on guns, bring your friends. It's fun to lose and to pretend.",
+        title: "Smells Like Teen Spirit"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "It's like the more money we come across, the more problems we see.",
+        title: "Mo’ Money Mo’ Problems"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "That's just the way it is. Things will never be the same.",
+        title: "Changes"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "Oh baby, baby, how was I supposed to know that something wasn't right here?",
+        title: "...Baby One More Time"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "Come on, vogue. Let your body go with the flow. You know you can do it.",
+        title: "Vogue"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "I thought I saw a man brought to life. He was warm, he came around like he was dignified.",
+        title: "Torn"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "Near, far, wherever you are, I believe that the heart does go on.",
+        title: "My Heart Will Go On"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: 'Say my name, say my name. If no one is around you, say “baby I love you"',
+        title: "Say My Name"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "You are my fire, the one desire. Believe when I say I want it that way",
+        title: "I Want It That Way"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "Time can't erase a feeling this strong. No way you're never gonna shake me.",
+        title: "Always Be My Baby"
     },
 
     8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
-    },
-
-    8: {
-        text: "Blackbird singing in the dead of night. Take these broken wings and learn to fly.",
-        info: "'Blackbird' by The Beatles - 1968"
+        text: "I want something else to get me through this semi-charmed kind of life, baby, baby.",
+        title: "Semi-Charmed Life"
     },
 };
 
