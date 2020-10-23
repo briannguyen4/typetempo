65307cc97797aba49f2c9c3388294f0c7a61183b
briannguyen4 <brian4nguyen@gmail.com>
2020-10-23 15:49:01 -0400

    seed more snippets

--- src/snippets.js
+++ src/snippets.js
@@ -51,191 +51,335 @@ export const snippets = {
         title: "'Like A Prayer' by Madonna - 1989"
     },
 
-    8: {
+    11: {
         text: "A singer in a smoky room, the smell of wine and cheap perfume.",        
         title: "Don't Stop Believin'"
     },
 
-    8: {
+    12: {
         text: "Oh! I wanna dance with somebody. I wanna feel the heat with somebody.",
         title: "I Wanna Dance With Somebody (Who Loves Me)"
     },
-    8: {
+
+    13: {
         text: "Oh momma dear, we're not the fortunate ones. And girls, they wanna have fun.",
         title: "Girls Just Want to Have Fun"
     },
 
-    8: {
+    14: {
         text: "Don't you know now is the perfect time. We can make it right, hit the city lights.",
         title: "P.Y.T. (Pretty Young Thing)"
     },
 
-    8: {
+    15: {
         text: "Jessie is a friend. Yeah, I know, he's been a good friend of mine.",
         title: "Jessie's Girl"
     },
 
-    8: {
+    16: {
         text: "I've been waiting for so long, now I've finally found someone to stand by me.",
         title: "(I’ve Had) The Time of My Life"
     },
 
-    8: {
+    17: {
         text: "A scrub is a guy that thinks he's fly. He's also known as a busta.",
         title: "No Scrubs"
     },
 
-    8: {
+    18: {
         text: "It's tearin' up my heart when I'm with you. But when we are apart, I feel it too.",
         title: "Tearin' Up My Heart"
     },
 
-    8: {
+    19: {
         text: "Load up on guns, bring your friends. It's fun to lose and to pretend.",
         title: "Smells Like Teen Spirit"
     },
 
-    8: {
+    20: {
         text: "It's like the more money we come across, the more problems we see.",
         title: "Mo’ Money Mo’ Problems"
     },
 
-    8: {
+    21: {
         text: "That's just the way it is. Things will never be the same.",
         title: "Changes"
     },
 
-    8: {
+    22: {
         text: "Oh baby, baby, how was I supposed to know that something wasn't right here?",
         title: "...Baby One More Time"
     },
 
-    8: {
+    23: {
         text: "I thought I saw a man brought to life. He was warm, he came around like he was dignified.",
         title: "Torn"
     },
 
-    8: {
+    24: {
         text: "Near, far, wherever you are, I believe that the heart does go on.",
         title: "My Heart Will Go On"
     },
 
-    8: {
+    25: {
         text: 'Say my name, say my name. If no one is around you, say “baby I love you"',
         title: "Say My Name"
     },
 
-    8: {
+    26: {
         text: "You are my fire, the one desire. Believe when I say I want it that way",
         title: "I Want It That Way"
     },
 
-    8: {
+    27: {
         text: "Time can't erase a feeling this strong. No way you're never gonna shake me.",
         title: "Always Be My Baby"
     },
 
-    8: {
+    28: {
         text: "I want something else to get me through this semi-charmed kind of life, baby, baby.",
         title: "Semi-Charmed Life"
     },
 
-    8: {
+    29: {
         text: "I know that I can't take no more, it ain't no lie. I wanna see you out that door.",
         title: "Bye Bye Bye"
     },
 
-    8: {
+    30: {
         text: "If you're havin' girl problems, I feel bad for you, son. I got 99 problems",
         title: "99 Problems"
     },
 
-    8: {
+    31: {
         text: "Windmill, windmill for the land. Turn forever, hand in hand.",
         title: "Feel Good Inc."
     },
 
-    8: {
+    32: {
         text: "Can't read my, can't read my, no, he can't read my poker face.",
         title: "Poker Face"
     },
 
-    8: {
+    33: {
         text: "I am beautiful no matter what they say. Words can't bring me down.",
         title: "Beautiful"
     },
 
-    8: {
+    34: {
         text: "But maybe I'm crazy, maybe you're crazy, maybe we're crazy, probably.",
         title: "Crazy"
     },
 
-    8: {
+    35: {
         text: "'Cause your love's got the best of me and baby you're making a fool of me.",
         title: "Crazy in Love"
     },
 
-    8: {
+    36: {
         text: "We are searchlights, we can see in the dark. We are rockets, pointed up at the stars.",
         title: "What About Us?"
     },
 
-    8: {
+    37: {
         text: "I'm on the pursuit of happiness, and I know everything that shine ain't always gonna be gold.",
         title: "Pursuit of Happiness"
     },
 
-    8: {
+    38: {
         text: "You better lose yourself in the music, the moment, you own it, you better never let it go, go.",
         title: "Lose Yourself"
     },
 
-    8: {
+    39: {
         text: "I didn't get a lot in class but I know it don't come in a shot glass.",
         title: "Rehab"
     },
 
-    8: {
+    40: {
         text: "I fly like paper, get high like planes. If you catch me at the border I got visas in my name.",
         title: "Paper Planes"
     },
     
-    8: {
+    41: {
         text: "I said, please don't slow me down if I'm going too fast. You're in a strange part of our town",
         title: "Reptilia"
     },
     
-    8: {
+    42: {
         text: `Last night, she said "Oh, baby, don't feel so down. Oh, it turns me off when I feel left out"`,
         title: "Last Night"
     },
     
-    8: {
+    43: {
         text: "Conversation got heavy. She had me feeling like she's ready to blow.",
         title: "Yeah!"
     },
     
-    8: {
+    44: {
         text: "Take control of your mind and meditate. Let your soul gravitate to the love, y'all.",
         title: "Where Is the Love?"
     },
     
-    8: {
+    45: {
         text: "Tonight's the night, let's live it up. I got my money, let's spend it up.",
         title: "I Gotta Feeling"
     },
     
-    8: {
+    46: {
         text: "Control yourself, take only what you need from it. A family of trees wanted to be haunted.",
         title: "Kids"
     },
 
-    8: {
+    47: {
         text: "Some people want it all but I don't want nothing at all if it ain't you, baby.",
         title: "If I Ain’t Got You"
     },
     
-    
+    48: {
+        text: "Just gonna stand there and watch me burn? Well, that's all right because I like the way it hurts.",
+        title: "Love the Way You Lie"
+    },
+
+    49: {
+        text: "Hey, I just met you, and this is crazy but here's my number, so call me maybe.",
+        title: "Call Me Maybe"
+    },
+
+    50: {
+        text: "You just gotta ignite the light and let it shine. Just own the night like the Fourth of July.",
+        title: "Firework"
+    },
+
+    51: {
+        text: "Same bed but it feels just a little bit bigger now. Our song on the radio but it don't sound the same.",
+        title: "When I Was Your MAn"
+    },
+
+    52: {
+        text: "Gold jewellery shining so bright. Strawberry champagne on ice.",
+        title: "That’s What I Like"
+    },
+
+    53: {
+        text: "You know you love me, I know you care. Just shout whenever and I’ll be there.",
+        title: "Baby"
+    },
+
+    54: {
+        text: "My mama don't like you and she likes everyone. And I never like to admit that I was wrong.",
+        title: "Love Yourself"
+    },
+
+    55: {
+        text: "You used to call me on my cell phone. Late-night when you need my love.",
+        title: "Hotline Bling"
+    },
+
+    56: {
+        text: "So it's gonna be forever or it's gonna go down in flames.",
+        title: "Blank Space"
+    },
+
+    57: {
+        text: "Hello from the other side. I must've called a thousand times.",
+        title: "Hello"
+    },
+
+    58: {
+        text: "I heard that you're settled down. That you found a girl and you're married now.",
+        title: "Someone Like You"
+    },
+
+    59: {
+        text: "My roof look like a no-show, got diamonds by the boatload.",
+        title: "Psycho"
+    },
+
+    60: {
+        text: "I'm at a payphone, trying to call home. All of my change, I spent on you.",
+        title: "Payphone"
+    },
+
+    61: {
+        text: "Look for the girl with the broken smile. Ask her if she wants to stay a while.",
+        title: "She Will Be Loved"
+    },
+
+    62: {
+        text: "And I know she'll be the death of me. At least we'll both be numb.",
+        title: "Can’t Feel My Face"
+    },
+
+    63: {
+        text: "White lips, pale face. Breathing in the snowflakes. Burnt lungs, sour taste.",
+        title: "The A Team"
+    },
+
+    64: {
+        text: "Girl, you know I want your love. Your love was handmade for somebody like me.",
+        title: "Shape of You"
+    },
+
+    65: {
+        text: "Now I know I have met an angel in person and she looks perfect.",
+        title: "Perfect"
+    },
+
+    66: {
+        text: "Baby, we don't stand a chance, it's sad but it's true. I'm way too good at goodbyes.",
+        title: "Too Good at Goodbyes"
+    },
+
+    67: {
+        text: "Well life will pass me by if I don't open up my eyes. Well that's fine by me.",
+        title: "Wake Me Up"
+    },
+
+    68: {
+        text: "Though we live on the US dollar. You and me, we got our own sense of time.",
+        title: "Hannah Hunt"
+    },
+
+    69: {
+        text: "I don't want your body but I hate to think about you with somebody else.",
+        title: "Somebody Else"
+    },
+
+    70: {
+        text: "I'm stronger now, I'm ready for the house. Such a modest mouse.",
+        title: "Step"
+    },
+
+    71: {
+        text: "I got my red dress on tonight. Dancin' in the dark in the pale moonlight.",
+        title: "Summertime Sadness"
+    },
+
+    72: {
+        text: "I got in a fight, I was indisposed. I was in despite all the wicked prose.",
+        title: "Kill V. Maim"
+    }, 
+
+    73: {
+        text: "Since you think I don't love you, I just thought you were cute, that's why I kissed you.",
+        title: "Thinkin Bout You"
+    },
+
+    74: {
+        text: "No matter what you say or what you do. When I'm alone, I'd rather be with you.",
+        title: "3005"
+    },
+
+    75: {
+        text: "And we'll never be royals, it don't run in our blood. That kind of luxe just ain't for us.",
+        title: "Royals"
+    },
+
+    76: {
+        text: "Come on, skinny love, just last the year. Pour a little salt, we were never here.",
+        title: "Skinny Love"
+    }
 };
 
 export const getSnippetsIdx = (n) => {
