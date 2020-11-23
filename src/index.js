66bb847e784e70b526fee7b3aa463c3695640041
briannguyen4 <brian4nguyen@gmail.com>
2020-11-23 16:52:33 -0500

    make changes

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles/index.scss";
-import "./gameplay";
-
-window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("play-container").style.display = "none";
-    document.getElementById("result-container").style.display = "none";
-});
-
